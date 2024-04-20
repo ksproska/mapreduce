77aeb9f43dd21336851a674050fc4bf664b16b86
Kamila Sproska <kamila.sproska@gmail.com>
2024-04-20 22:40:36 +0200

    reviews works

--- src/main/java/org/example/ReviewsSubseter.java
+++ src/main/java/org/example/ReviewsSubseter.java
@@ -13,19 +13,42 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
 
-public class ReviewsSubseter {
+public class ReviewsSubseter { // listing_id,id,date,reviewer_id,reviewer_name,comments
+
+    public static final int listing_id_inx = 0;
+    public static final int id_inx = 1;
+    public static final int comments_inx = 5;
+
     private static class Map extends Mapper<LongWritable, Text, Text, Text> {
         @Override
         public void map(LongWritable Text, Text value, Context context) throws IOException, InterruptedException {
-
             String[] line = value.toString().split(",");
-            if (line.length > 1) {
-                org.apache.hadoop.io.Text keyout = new Text(line[0].trim());
-                org.apache.hadoop.io.Text valueout = new Text(line[1].trim());
-                context.write(keyout, valueout);
+            org.apache.hadoop.io.Text keyout = new Text(line[listing_id_inx].trim());
+            String id = "";
+            String comments = "";
+
+            if (line.length > id_inx) {
+                id = line[id_inx].trim();
             } else {
                 System.out.println("Skipping line: " + value.toString());
             }
+            if (line.length > comments_inx) {
+                StringBuilder commentsBuilder = new StringBuilder();
+                int inx = comments_inx;
+                while (inx < line.length) {
+                    commentsBuilder.append(line[inx]);
+                    inx += 1;
+                }
+                comments = commentsBuilder.toString();
+                comments = comments.replace("<br/>", "");
+                comments = comments.replace("\"", "");
+                comments = comments.replace("\n", "");
+            } else {
+                System.out.println("Skipping line: " + value.toString());
+            }
+
+            org.apache.hadoop.io.Text valueout = new Text(id + "," + comments);
+            context.write(keyout, valueout);
         }
     }
 
