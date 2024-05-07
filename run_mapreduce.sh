66a4ec7e3ed4fb55fb85f7bcb7ea02c963cde695
Numpek2k <theinqu@gmail.com>
2024-05-07 19:27:52 +0200

    script update

--- run_mapreduce.sh
+++ run_mapreduce.sh
@@ -28,3 +28,60 @@ hadoop jar events.jar EventsSubseter /new-york/NYC_events_filtered.csv /new-york
 hdfs dfs -ls /new-york/subset/events
 hadoop fs -cat /new-york/subset/events/* | head
 
+#ATTEMPT 2
+
+hadoop com.sun.tools.javac.Main EventFilter.java
+jar cf events.jar EventFilter*.class
+hdfs dfs -rm -r -f /new-york/subset/events
+hadoop jar events.jar EventFilter /new-york/NYC_events_filtered.csv /new-york/subset/events
+hdfs dfs -ls /new-york/subset/events
+hadoop fs -cat /new-york/subset/events/* | head
+
+hadoop com.sun.tools.javac.Main EventCount.java
+jar cf events.jar EventCount*.class
+hdfs dfs -rm -r -f /new-york/subset/events_count
+hadoop jar events.jar EventCount /new-york/subset/events/part-r-00000 /new-york/subset/events_count
+hdfs dfs -ls /new-york/subset/events_count
+hadoop fs -cat /new-york/subset/events_count/* | head
+
+hadoop com.sun.tools.javac.Main Etap1.java
+jar cf etap1.jar Etap1*.class
+hdfs dfs -rm -r -f /new-york/subset/E1
+hadoop jar etap1.jar Etap1 /new-york/calendar.csv /new-york/listings_detailed_cleaned.csv /new-york/subset/E1
+hdfs dfs -ls /new-york/subset/E1
+hadoop fs -cat /new-york/subset/E1/* | head
+
+hadoop com.sun.tools.javac.Main Etap2.java
+jar cf etap1.jar Etap2*.class
+hdfs dfs -rm -r -f /new-york/subset/E2
+hadoop jar etap1.jar Etap2 /new-york/subset/E1/part-r-00000 /new-york/subset/E2
+hdfs dfs -ls /new-york/subset/E2
+hadoop fs -cat /new-york/subset/E2/* | head
+
+hadoop com.sun.tools.javac.Main Etap3.java
+jar cf etap1.jar Etap3*.class
+hdfs dfs -rm -r -f /new-york/subset/E3
+hadoop jar etap1.jar Etap3 /new-york/subset/events_count/part-r-00000 /new-york/subset/E2/part-r-00000 /new-york/subset/E3
+hdfs dfs -ls /new-york/subset/E3
+hadoop fs -cat /new-york/subset/E3/* | head
+
+hadoop com.sun.tools.javac.Main Etap3.java
+jar cf etap1.jar Etap3*.class
+hdfs dfs -rm -r -f /new-york/subset/E3
+hadoop jar etap1.jar Etap3 /new-york/subset/events_count/part-r-00000 /new-york/subset/E2/part-r-00000 /new-york/subset/E3
+hdfs dfs -ls /new-york/subset/E3
+hadoop fs -cat /new-york/subset/E3/* | head
+
+hadoop com.sun.tools.javac.Main Etap4.java
+jar cf etap1.jar Etap4*.class
+hdfs dfs -rm -r -f /new-york/subset/E4
+hadoop jar etap1.jar Etap4 /new-york/subset/E3/part-r-00000 /new-york/subset/E4
+hdfs dfs -ls /new-york/subset/E4
+hadoop fs -cat /new-york/subset/E4/* | head
+
+hadoop com.sun.tools.javac.Main Etap5.java
+jar cf etap1.jar Etap5*.class
+hdfs dfs -rm -r -f /new-york/subset/E5
+hadoop jar etap1.jar Etap5 /new-york/weather_data.csv /new-york/subset/E4/part-r-00000 /new-york/subset/E5
+hdfs dfs -ls /new-york/subset/E5
+hadoop fs -cat /new-york/subset/E5/* | head
