61ab3c000bcefeb62881cd2f74fe6f3cc6ba205c
Kamila Sproska <kamila.sproska@gmail.com>
2024-06-08 12:41:59 +0200

    phase 4 and 5

---
+++ hive/f04_etap2.sql
@@ -0,0 +1,26 @@
+use nyc_data;
+
+CREATE OR REPLACE VIEW transformed_data AS
+SELECT
+    listing_id,
+    calendar_date,
+    CAST(REGEXP_REPLACE(REGEXP_REPLACE(price, '\\$', ''), ',', '') AS DOUBLE) AS price,
+    neighbourhood_group_cleansed,
+    room_type
+FROM etap1_data;
+
+CREATE OR REPLACE VIEW grouped_data AS
+SELECT
+    calendar_date,
+    neighbourhood_group_cleansed,
+    room_type,
+    ROUND(AVG(price), 2) AS average_price
+FROM transformed_data
+GROUP BY calendar_date, neighbourhood_group_cleansed, room_type;
+
+CREATE TABLE etap2_data AS
+SELECT *
+FROM grouped_data
+WHERE average_price > 0.0;
+
+SELECT * FROM etap2_data;
---
+++ hive/f05_etap3.sql
@@ -0,0 +1,16 @@
+use nyc_data;
+
+CREATE TABLE etap3_data AS
+SELECT f.calendar_date,
+       f.neighbourhood_group_cleansed,
+       f.room_type,
+       f.average_price,
+       e.total_count
+FROM etap2_data f
+         JOIN
+     events_summed e
+     ON
+         f.calendar_date = e.event_date AND
+         f.neighbourhood_group_cleansed = e.event_borough;
+
+SELECT * FROM etap3_data;
