64e6acdf832b8f16b33e7a9a835c723065ce34bb
Kamila Sproska <kamila.sproska@gmail.com>
2024-06-17 22:21:02 +0200

    etap4 works

--- spark/f06_etap4.scala
+++ spark/f06_etap4.scala
@@ -11,11 +11,11 @@ val holidayDatesSchema = StructType(Array(
 
 val holidayDates = spark.read.format("csv").option("header", "false").option("sep", ",").option("quote", "\"").option("escape", "\"").schema(holidayDatesSchema).load("/new-york/dates.csv")
 
-val etap3Data = spark.table("nyc_data.etap3_data")
+val etap3Data = spark.table("etap3_data")
 
-val etap4Data = etap3Data.withColumn("calendar_date", to_date(col("calendar_date"), "yyyy-MM-dd")).join(holidayDates.withColumnRenamed("holiday_date", "holiday"), $"calendar_date" === $"holiday", "left_outer").withColumn("is_weekend", expr("dayofweek(calendar_date) IN (1, 7)")).withColumn("is_holiday", col("holiday").isNotNull).select( $"calendar_date", $"neighbourhood_group_cleansed", $"room_type", $"average_price", $"sum", $"is_weekend", $"is_holiday")
+val etap4Data = etap3Data.withColumn("calendar_date", to_date(col("calendar_date"), "yyyy-MM-dd")).join(holidayDates.withColumnRenamed("holiday_date", "holiday"), $"calendar_date" === $"holiday", "left_outer").withColumn("is_weekend", expr("dayofweek(calendar_date) IN (1, 7)")).withColumn("is_holiday", col("holiday").isNotNull).select( $"calendar_date", $"neighbourhood_group_cleansed", $"room_type", $"average_price", $"total_count", $"is_weekend", $"is_holiday")
 
-etap4Data.write.format("hive").mode("overwrite").saveAsTable("nyc_data.etap4_data")
+etap4Data.write.format("hive").mode("overwrite").saveAsTable("etap4_data")
 
 val totalTime = (System.currentTimeMillis - before) / 1000
 System.out.println(s"Elapsed (sec): $totalTime")
