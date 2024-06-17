4f803d5bce98c7b95efaa22d8ca514d334b4ab54
Kamila Sproska <kamila.sproska@gmail.com>
2024-06-17 22:20:40 +0200

    etap4 workis

--- spark/f06_etap4.scala
+++ spark/f06_etap4.scala
@@ -9,9 +9,7 @@ val holidayDatesSchema = StructType(Array(
   StructField("holiday_date", StringType, true)
 ))
 
-val holidayDates = spark.read.format("csv").option("header", "true").option("sep", ",").option("quote", "\"").option("escape", "\"").schema(holidayDatesSchema).load("/new-york/dates.csv")
-
-holidayDates.write.format("hive").option("path", "/hive/path/for/holiday_dates").mode("overwrite").saveAsTable("nyc_data.holiday_dates")
+val holidayDates = spark.read.format("csv").option("header", "false").option("sep", ",").option("quote", "\"").option("escape", "\"").schema(holidayDatesSchema).load("/new-york/dates.csv")
 
 val etap3Data = spark.table("nyc_data.etap3_data")
 
